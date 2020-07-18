323cef92a61272934f30968be621d3c753442a22
Victor <marcelo.victor05@gmail.com>
2020-07-18 12:40:41 -0300

    feat: horizontal scroll on the board

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-	width: 100%;
-	border: 2px solid red;
-	height: calc(100vh - 50px);
+	min-width: 100%;
+	width: max-content;
+	height: calc(100vh - 45px);
 	padding: 8px;
 	background: ${({ theme }) => theme.mainColor};
 	display: flex;
-	overflow-y: hidden;
+	overflow-y: hidden;	
 	overflow-x: auto;
+	margin-top: 45px;
 `;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
     width: 100%;
-    height: 50px;
+    height: 45px;
     padding: 0 32px;
     display: flex;
     align-items: center;
     justify-content: space-between;
     background: ${({ theme }) => theme.mainColor}99;
-
+    position: fixed;
+    
     & > h1 {
         color: #FFF;
         font-size: 18px;
